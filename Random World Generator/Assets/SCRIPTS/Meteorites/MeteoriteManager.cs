52f1c19a6e63e63bbbacf7ec2c8ff1a0bcad8f34
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-03-25 18:40:49 +0200

    Basic MeteoriteMaker and -Manager working

--- Random World Generator/Assets/SCRIPTS/Meteorites/MeteoriteManager.cs
+++ Random World Generator/Assets/SCRIPTS/Meteorites/MeteoriteManager.cs
@@ -29,19 +29,58 @@ public class MeteoriteManager : MonoBehaviour
     //private void MakeMeteorite(Vector2 _pos, Vector2 _target, float _speed)
     private void MakeMeteorite()
     {
+        // Get birth Position
+        Transform birthPos;
+        birthPos = randomPoints[Random.Range(0, randomPoints.Length)].transform;
+
+        // Get Target Pos   (1/2)
+        Transform targetPos;
+        bool targetFound = false;
+
+        // Set initial target Pos and test if it is the same as birthPos    (2/2)
+        targetPos = randomPoints[Random.Range(0, randomPoints.Length)].transform;
+        while(!targetFound)
+        {
+
+            if (targetPos != birthPos)
+            {
+                targetFound = true;
+                break;
+            }
+
+            // Try again
+            targetPos = randomPoints[Random.Range(0, randomPoints.Length)].transform;
+
+            // Test if the randomPoints array is empty
+            if (randomPoints.Length <= 0)
+            {
+                Debug.LogError("Random Points list is empty!");
+                targetPos = gameObject.transform;
+            }
+        }
+
+
         // Instantiate at RandomPoint
-        GameObject thisMeteorite = Instantiate(meteorites[0], randomPoints[Random.Range(0, randomPoints.Length)].transform.position, Quaternion.identity) as GameObject;
+        GameObject thisMeteorite = Instantiate(meteorites[0], birthPos.position, Quaternion.identity) as GameObject;
 
         // Calculate Fly Direction
-
+        Vector2 flyDirection = targetPos.position - birthPos.position;
+        Debug.Log("TargetPos: " + targetPos.position);
+        Debug.Log("BirthPos: " + birthPos.position);
 
         // Give Speed and Shoot
         Rigidbody2D rb = thisMeteorite.GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.left * GetRandomSpeed());
+        //rb.AddForce(Vector2.left * GetRandomSpeed());     // Old, delete
+        rb.AddForce(flyDirection * GetRandomSpeed());
     }
 
     private float GetRandomSpeed()
     {
         return Random.Range(minSpeed, maxSpeed);
     }
+
+    //private Transform GetTarget(GameObject birthPos)
+    //{
+        
+    //}
 }
