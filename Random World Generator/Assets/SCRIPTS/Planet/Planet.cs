ce025306e0c84ba01358e226dd20ad36ab04bb5b
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-03-26 09:35:28 +0200

    Made some Lists

--- Random World Generator/Assets/SCRIPTS/Planet/Planet.cs
+++ Random World Generator/Assets/SCRIPTS/Planet/Planet.cs
@@ -10,17 +10,36 @@ public class Planet : MonoBehaviour
 
     [SerializeField] GameObject TEST_OBJECT;
 
+    [Header("Placement List")]
+    [SerializeField] int placementAmount = 36;
+    [SerializeField] List<int> placeList = new List<int> ();
+
 
     void Start()
     {
-        
+        for (int i = 0; i < placementAmount; i++)
+        {
+            placeList.Add(0);
+        }
     }
-
-
     void Update()
     {
         RotatePlanet();
     }
+
+    // LISTS
+
+    public int GetPos(int pos)
+    {
+        return placeList[pos];
+    }
+
+    public int GetListLength()
+    {
+        return placeList.Count;
+    }
+
+
 
     // ROTATION
 
@@ -40,6 +59,7 @@ public class Planet : MonoBehaviour
         DoCollisionEvent(collision.gameObject);
     }
 
+
     private void DoCollisionEvent(GameObject collider)
     {
         // Calculate the angle between Planet and collider
@@ -48,10 +68,44 @@ public class Planet : MonoBehaviour
         // Calculate the correct distance for the Instantiated GameObject
         //float planetRadius = 0.4f;
 
+        float debugAngle = Mathf.RoundToInt((angle / 360) * GetListLength());
+        Debug.Log("DebugAngle: " + debugAngle);
+
         Vector2 desiredPos = transform.position;
         desiredPos = Vector2.MoveTowards(transform.position, collider.transform.position, planetRadius);
 
-        Debug.Log("DesiredPos: " + desiredPos);
+        //Debug.Log("DesiredPos: " + desiredPos);
+
+        // Add something as a child on the Planet's surface
+        //GameObject thingy = Instantiate(TEST_OBJECT, collider.transform.position, Quaternion.identity) as GameObject;
+        GameObject thingy = Instantiate(TEST_OBJECT, desiredPos, Quaternion.identity) as GameObject;
+
+        // Turn the instantiated surface gameobject to the correct rotation
+        thingy.transform.eulerAngles = new Vector3(0, 0, angle - 90);
+
+        // Make the Planet the Parent
+        thingy.transform.parent = gameObject.transform;
+
+        // Destroy the Meteorite object
+        //Destroy(collider);
+        collider.SetActive(false);
+    }
+
+    private void EXAMPLE(GameObject collider)
+    {
+        // Calculate the angle between Planet and collider
+        float angle = Mathf.Atan2(collider.transform.position.y - transform.position.y, collider.transform.position.x - transform.position.x) * 180 / Mathf.PI;
+
+        // Calculate the correct distance for the Instantiated GameObject
+        //float planetRadius = 0.4f;
+
+        float debugAngle = Mathf.RoundToInt((angle / 360) * GetListLength());
+        Debug.Log("DebugAngle: " + debugAngle);
+
+        Vector2 desiredPos = transform.position;
+        desiredPos = Vector2.MoveTowards(transform.position, collider.transform.position, planetRadius);
+
+        //Debug.Log("DesiredPos: " + desiredPos);
 
         // Add something as a child on the Planet's surface
         //GameObject thingy = Instantiate(TEST_OBJECT, collider.transform.position, Quaternion.identity) as GameObject;
