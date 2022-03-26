f7e7f9ec5070e06a3ab4f6732a7af7b6cd4014bc
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-03-26 22:05:40 +0200

    More Particles

--- Random World Generator/Assets/SCRIPTS/Graphics/ParticleMaker.cs
+++ Random World Generator/Assets/SCRIPTS/Graphics/ParticleMaker.cs
@@ -28,4 +28,20 @@ public class ParticleMaker : MonoBehaviour
         _particle.transform.parent = null;
         Destroy(_particle, maxLifetime);
     }
+
+    // RESERVE, maybe necessary for Meteor to have it's own Angle Particle Maker
+    //public void MakeParticlesAtAngle(int whichNumber, float angle)
+    //{
+    //    GameObject _particle = Instantiate(particles[whichNumber], gameObject.transform) as GameObject;
+
+    //    // Rotate
+    //    //_particle.transform.eulerAngles = new Vector3(-90, 0, angle);
+    //    _particle.transform.eulerAngles = new Vector3((angle), -90, 90);
+    //    Debug.Log("True Angle: " + _particle.transform.eulerAngles.x);
+
+    //    Debug.Log("Given Angle: " + angle);
+
+    //    _particle.transform.parent = null;
+    //    Destroy(_particle, maxLifetime);
+    //}
 }
