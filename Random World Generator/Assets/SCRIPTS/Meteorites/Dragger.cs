a8359206143c86417992925e77a505c36ab7971a
MiksuMaker <91052332+MiksuMaker@users.noreply.github.com>
2022-03-26 13:38:09 +0200

    Changed Dragger and began Gravity

--- Random World Generator/Assets/SCRIPTS/Meteorites/Dragger.cs
+++ Random World Generator/Assets/SCRIPTS/Meteorites/Dragger.cs
@@ -58,158 +58,165 @@ public class Dragger : MonoBehaviour
         // Add the Force into correct direction
         rb.AddForce(dragDirection * GetForceAmount(dist), ForceMode2D.Force);
 
-        //rb.AddForce(flyDirection * GetRandomSpeed(), ForceMode2D.Impulse);
+        void FixedUpdate()
+        {
+            // Reduce Velocity
+            if (rb.velocity.magnitude > 10f)
+            {
+                rb.velocity = rb.velocity.normalized * maxForce;
+            }
+        }
+
+        float GetForceAmount(float dist)
+        {
+            float force;
+
+            if (dist <= minDistance)
+            {
+                // No force
+                force = 0f;
+                return force;
+            }
+            else if (distance >= maxDistance)
+            {
+                force = maxForce;
+                return force;
+            }
+            else
+            {
+                // Calculate the force according to Distance
+                force = dist * 1.5f;
+                return force;
+            }
+        }
+
+
+
+
+        // CARGO CULT THIEVERY
+
+        // Class Variables
+        //public float distance = 0.2f;
+        //public float damper = 0.5f; // damping ration in SpringJoint2D (0.0.- 1.0)
+        //public float frequency = 8.0f;
+        //public float drag = 1.0f; // this doesn't exist on 2D Spring...
+        //public float angularDrag = 5.0f;
+        //var distance = 0.2;
+        //public bool attachToCenterOfMass = false;
+        //private SpringJoint2D springJoint;
+
+
+        //private void Start()
+        //{
+        //    camera = FindCamera();
+        //}
+
+
+        // Update
+        //void Update()
+        //{
+
+
+        //    if (!Input.GetMouseButtonDown(0))
+        //        return;
+
+
+        //    int layerMask = 1 << 7;
+        //    RaycastHit2D hit = Physics2D.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
+
+        //    Debug.Log("Layermask: " + LayerMask.LayerToName(6));
+        //     I have proxy collider objects (empty gameobjects with a 2D Collider) as a child of a 3D rigidbody - simulating collisions between 2D and 3D objects
+        //     I therefore set any 'touchable' object to layer 8 and use the layerMask above for all touchable items
+
+        //    if (hit.collider != null && hit.rigidbody.isKinematic == true) {
+        //        return;
+        //    }
+
+        //    if (hit.collider != null && hit.rigidbody.isKinematic == false) {
+
+
+        //        if (!springJoint)
+        //        {
+        //            GameObject go = new GameObject("Rigidbody2D Dragger");
+        //            Rigidbody2D body = go.AddComponent("Rigidbody2D") as Rigidbody2D;
+        //            Rigidbody2D body = go.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        //            springJoint = go.AddComponent<SpringJoint2D>() as SpringJoint2D;
+
+        //            body.isKinematic = true;
+        //        }
+
+        //        springJoint.transform.position = hit.point;
+
+
+        //        if (attachToCenterOfMass)
+        //        {
+
+        //            Debug.Log("Currently 'centerOfMass' isn't reported for 2D physics like 3D Physics - it will be added in a future release.");
+        //             Currently 'centerOfMass' isn't reported for 2D physics like 3D Physics yet - it will be added in a future release.
+
+        //            Vector3 anchor = transform.TransformDirection(hit.rigidbody.centerOfMass) + hit.rigidbody.transform.position; in c# might be Vector2?
+
+        //            anchor = springJoint.transform.InverseTransformPoint(anchor);
+        //            springJoint.anchor = anchor;
+        //        }
+        //        else
+        //        {
+
+        //            springJoint.anchor = Vector3.zero;
+        //        }
+
+        //        springJoint.distance = distance; // there is no distance in SpringJoint2D
+        //        springJoint.dampingRatio = damper;// there is no damper in SpringJoint2D but there is a dampingRatio
+        //                                          /*springJoint.maxDistance = distance;*/  // there is no MaxDistance in the SpringJoint2D - but there is a 'distance' field
+        //                                            see http://docs.unity3d.com/Documentation/ScriptReference/SpringJoint2D.html
+        //                                          springJoint.maxDistance = distance;
+        //        springJoint.connectedBody = hit.rigidbody;
+
+
+        //         maybe check if the 'fraction' is normalised. See http://docs.unity3d.com/Documentation/ScriptReference/RaycastHit2D-fraction.html
+        //        StartCoroutine("DragObject", hit.fraction);
+
+
+
+        //    } // end of hit true condition
+
+        //} // end of update
+
+
+        //IEnumerator DragObject(float distance)
+        //{
+
+        //    float oldDrag = springJoint.connectedBody.drag;
+        //    float oldAngularDrag = springJoint.connectedBody.angularDrag;
+
+        //    springJoint.connectedBody.drag = drag;
+        //    springJoint.connectedBody.angularDrag = angularDrag;
+
+
+        //    while (Input.GetMouseButton(0))
+        //    {
+        //        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        //        springJoint.transform.position = ray.GetPoint(distance);
+        //        yield return null;
+        //    }
+
+
+
+        //    if (springJoint.connectedBody)
+        //    {
+        //        springJoint.connectedBody.drag = oldDrag;
+        //        springJoint.connectedBody.angularDrag = oldAngularDrag;
+        //        springJoint.connectedBody = null;
+        //    }
+
+        //}
+
+        //Camera FindCamera()
+        //{
+        //    if (camera)
+        //        return camera;
+        //    else
+        //        return Camera.main;
+        //}
     }
-
-    private float GetForceAmount(float dist)
-    {
-        float force;
-
-        if (dist <= minDistance)
-        {
-            // No force
-            force = 0f;
-            return force;
-        }
-        else if (distance >= maxDistance)
-        {
-            force = maxForce;
-            return force;
-        }
-        else
-        {
-            // Calculate the force according to Distance
-            force = dist * 1.5f;
-            return force;
-        }
-    }
-
-
-
-
-    // CARGO CULT THIEVERY
-
-    // Class Variables
-    //public float distance = 0.2f;
-    //public float damper = 0.5f; // damping ration in SpringJoint2D (0.0.- 1.0)
-    //public float frequency = 8.0f;
-    //public float drag = 1.0f; // this doesn't exist on 2D Spring...
-    //public float angularDrag = 5.0f;
-    //var distance = 0.2;
-    //public bool attachToCenterOfMass = false;
-    //private SpringJoint2D springJoint;
-
-
-    //private void Start()
-    //{
-    //    camera = FindCamera();
-    //}
-
-
-    // Update
-    //void Update()
-    //{
-
-
-    //    if (!Input.GetMouseButtonDown(0))
-    //        return;
-
-
-    //    int layerMask = 1 << 7;
-    //    RaycastHit2D hit = Physics2D.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
-
-    //    Debug.Log("Layermask: " + LayerMask.LayerToName(6));
-    //     I have proxy collider objects (empty gameobjects with a 2D Collider) as a child of a 3D rigidbody - simulating collisions between 2D and 3D objects
-    //     I therefore set any 'touchable' object to layer 8 and use the layerMask above for all touchable items
-
-    //    if (hit.collider != null && hit.rigidbody.isKinematic == true) {
-    //        return;
-    //    }
-
-    //    if (hit.collider != null && hit.rigidbody.isKinematic == false) {
-
-
-    //        if (!springJoint)
-    //        {
-    //            GameObject go = new GameObject("Rigidbody2D Dragger");
-    //            Rigidbody2D body = go.AddComponent("Rigidbody2D") as Rigidbody2D;
-    //            Rigidbody2D body = go.AddComponent<Rigidbody2D>() as Rigidbody2D;
-    //            springJoint = go.AddComponent<SpringJoint2D>() as SpringJoint2D;
-
-    //            body.isKinematic = true;
-    //        }
-
-    //        springJoint.transform.position = hit.point;
-
-
-    //        if (attachToCenterOfMass)
-    //        {
-
-    //            Debug.Log("Currently 'centerOfMass' isn't reported for 2D physics like 3D Physics - it will be added in a future release.");
-    //             Currently 'centerOfMass' isn't reported for 2D physics like 3D Physics yet - it will be added in a future release.
-
-    //            Vector3 anchor = transform.TransformDirection(hit.rigidbody.centerOfMass) + hit.rigidbody.transform.position; in c# might be Vector2?
-
-    //            anchor = springJoint.transform.InverseTransformPoint(anchor);
-    //            springJoint.anchor = anchor;
-    //        }
-    //        else
-    //        {
-
-    //            springJoint.anchor = Vector3.zero;
-    //        }
-
-    //        springJoint.distance = distance; // there is no distance in SpringJoint2D
-    //        springJoint.dampingRatio = damper;// there is no damper in SpringJoint2D but there is a dampingRatio
-    //                                          /*springJoint.maxDistance = distance;*/  // there is no MaxDistance in the SpringJoint2D - but there is a 'distance' field
-    //                                            see http://docs.unity3d.com/Documentation/ScriptReference/SpringJoint2D.html
-    //                                          springJoint.maxDistance = distance;
-    //        springJoint.connectedBody = hit.rigidbody;
-
-
-    //         maybe check if the 'fraction' is normalised. See http://docs.unity3d.com/Documentation/ScriptReference/RaycastHit2D-fraction.html
-    //        StartCoroutine("DragObject", hit.fraction);
-
-
-
-    //    } // end of hit true condition
-
-    //} // end of update
-
-
-    //IEnumerator DragObject(float distance)
-    //{
-
-    //    float oldDrag = springJoint.connectedBody.drag;
-    //    float oldAngularDrag = springJoint.connectedBody.angularDrag;
-
-    //    springJoint.connectedBody.drag = drag;
-    //    springJoint.connectedBody.angularDrag = angularDrag;
-
-
-    //    while (Input.GetMouseButton(0))
-    //    {
-    //        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-    //        springJoint.transform.position = ray.GetPoint(distance);
-    //        yield return null;
-    //    }
-
-
-
-    //    if (springJoint.connectedBody)
-    //    {
-    //        springJoint.connectedBody.drag = oldDrag;
-    //        springJoint.connectedBody.angularDrag = oldAngularDrag;
-    //        springJoint.connectedBody = null;
-    //    }
-
-    //}
-
-    //Camera FindCamera()
-    //{
-    //    if (camera)
-    //        return camera;
-    //    else
-    //        return Camera.main;
-    //}
 }
