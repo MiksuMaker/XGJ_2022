fccd91d59b12b7d254ccd35d5a50ce80ddd6b07d
gitisti <76721708+gitisti@users.noreply.github.com>
2022-03-27 11:20:30 +0300

    kommit

--- Random World Generator/Assets/FadeOut.cs
+++ Random World Generator/Assets/FadeOut.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class FadeOut : MonoBehaviour
 {
     [SerializeField] SpriteRenderer sr;
-
+    bool fading = false;
     private void Update()
     {
-        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a - .6f * Time.deltaTime);
-        if (sr.color.a <= 0f)
+        if (fading)
         {
-            gameObject.SetActive(false);
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a - .6f * Time.deltaTime);
+            if (sr.color.a <= 0f)
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
+
+
+    public void startFade()
+    {
+        fading = true;
+    }
+
 }
